de070e655a8c9b42b94f9a6fa18e681796004ed1
Kaifler, Pau <pau.kaifler@hensoldt.net>
2023-10-10 11:55:06 +0200

    basic sender

--- tcp_throughput_c2s_client/src/main.rs
+++ tcp_throughput_c2s_client/src/main.rs
@@ -1,31 +1,36 @@
 use std::{
     io::{Read, Write},
-    net::TcpListener,
+    net::TcpStream,
+    time::{Duration, Instant},
 };
 
 use color_eyre::Result;
+use rand::{RngCore, SeedableRng};
 
-const COOKIE_SIZE: usize = 37;
-const TCP_BLKSIZE: usize = 128 * 1024;
+const DURATION: Duration = Duration::from_secs(10);
+const BLOCK_SIZE: usize = 128 * 1024;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
 
-    let listener = TcpListener::bind("127.0.0.1:8888")?;
-    let (mut stream, _) = listener.accept()?;
+    let mut rng = rand_xoshiro::Xoroshiro128Plus::from_entropy();
+    let mut write_buf = vec![0; BLOCK_SIZE];
+    rng.fill_bytes(&mut write_buf);
+    let mut read_buf = vec![0; BLOCK_SIZE];
 
-    {
-        let mut cookie_buf = [0; COOKIE_SIZE];
-        stream.read_exact(&mut cookie_buf)?;
-        println!("{}", std::str::from_utf8(&cookie_buf)?);
+    let start_time = Instant::now();
+    let mut stream = TcpStream::connect("127.0.0.1:8888")?;
+    stream.set_nodelay(true)?;
+    let mut bytes_transmitted = 0;
+    while start_time.elapsed() < DURATION {
+        stream.write(&write_buf)?;
+        // stream.read_exact(&mut read_buf)?;
+        bytes_transmitted += BLOCK_SIZE;
     }
 
-    {
-        let mut buf = vec![0; TCP_BLKSIZE];
-        while let Ok(n) = stream.read(&mut buf) {
-            stream.write(&buf[..n])?;
-        }
-    }
+    let elapsed = start_time.elapsed();
+    let bytes_per_second = ((bytes_transmitted as f64) / elapsed.as_secs_f64()) as u64;
+    println!("{}/s", bytesize::to_string(bytes_per_second, true));
 
     Ok(())
 }
