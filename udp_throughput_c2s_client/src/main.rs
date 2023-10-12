d443263e3f49a3154e386a295381ac9135bbc56f
Kaifler, Pau <pau.kaifler@hensoldt.net>
2023-10-12 13:07:35 +0200

    Take address from arguments

--- udp_throughput_c2s_client/src/main.rs
+++ udp_throughput_c2s_client/src/main.rs
@@ -1,30 +1,38 @@
 use std::{
-    io::{Read, Write},
+    io::Write,
     net::TcpStream,
     time::{Duration, Instant},
 };
 
+use argh::FromArgs;
 use color_eyre::Result;
 use rand::{RngCore, SeedableRng};
 
 const DURATION: Duration = Duration::from_secs(10);
 const BLOCK_SIZE: usize = 128 * 1024;
 
+/// A tool to benchmark network applications.
+#[derive(FromArgs)]
+struct Args {
+    /// target address and port, delimited by a colon
+    #[argh(option, default = "String::from(\"10.0.0.10:5560\")")]
+    address: String,
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
+    let args: Args = argh::from_env();
 
     let mut rng = rand_xoshiro::Xoroshiro128Plus::from_entropy();
     let mut write_buf = vec![0; BLOCK_SIZE];
     rng.fill_bytes(&mut write_buf);
-    let mut read_buf = vec![0; BLOCK_SIZE];
 
     let start_time = Instant::now();
-    let mut stream = TcpStream::connect("127.0.0.1:8888")?;
+    let mut stream = TcpStream::connect(&args.address)?;
     stream.set_nodelay(true)?;
     let mut bytes_transmitted = 0;
     while start_time.elapsed() < DURATION {
         stream.write(&write_buf)?;
-        // stream.read_exact(&mut read_buf)?;
         bytes_transmitted += BLOCK_SIZE;
     }
 
