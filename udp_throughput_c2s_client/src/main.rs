30e294201c9b7e27494edd1d3e0568a476013cb3
Kaifler, Pau <pau.kaifler@hensoldt.net>
2023-10-17 16:21:14 +0200

    Switch to socket2 and usability improvements

--- udp_throughput_c2s_client/src/main.rs
+++ udp_throughput_c2s_client/src/main.rs
@@ -1,16 +1,27 @@
 use std::{
-    collections::VecDeque,
-    net::UdpSocket,
+    io::Read,
+    net::ToSocketAddrs,
+    str::FromStr,
     time::{Duration, Instant},
 };
 
 use argh::FromArgs;
 use bytesize::ByteSize;
-use color_eyre::Result;
+use color_eyre::{eyre::eyre, Result};
 use rand::{RngCore, SeedableRng};
+use socket2::{Domain, Protocol, Socket, Type};
 
 fn parse_duration(s: &str) -> Result<Duration, String> {
     humantime::parse_duration(s).map_err(|err| err.to_string())
+}
+
+fn parse_rate(full: &str) -> Result<u64, String> {
+    let without_second = full.strip_suffix("/s").unwrap_or(full);
+    if let Some(bits) = without_second.strip_suffix("bit") {
+        Ok(ByteSize::from_str(bits)?.0 / 8)
+    } else {
+        Ok(ByteSize::from_str(without_second)?.0)
+    }
 }
 
 /// A tool to benchmark network applications.
@@ -29,9 +40,9 @@ struct Args {
     /// how large each packet should be
     #[argh(option, short = 'n', default = "ByteSize::b(1472)")]
     packet_size: ByteSize,
-    /// approximately how many bytes should be sent per second
-    #[argh(option, short = 'b', default = "ByteSize::kb(100)")]
-    bytes_per_second: ByteSize,
+    /// approximately how many bits (`bit` suffix) or bytes (`B` suffix) should be sent per second
+    #[argh(option, short = 'b', default = "1024 * 1024", from_str_fn(parse_rate))]
+    rate: u64,
 }
 
 fn main() -> Result<()> {
@@ -41,15 +52,23 @@ fn main() -> Result<()> {
     let mut rng = rand_xoshiro::Xoroshiro128Plus::from_entropy();
     let mut buf = vec![0; args.packet_size.0 as usize];
 
-    let socket = UdpSocket::bind("0.0.0.0:0")?;
-    socket.connect(args.address)?;
+    let address = args
+        .address
+        .to_socket_addrs()?
+        .next()
+        .ok_or_else(|| eyre!("Could not resolve address"))?
+        .into();
+
+    let mut socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
+    socket.connect(&address)?;
+    socket.set_read_timeout(Some(Duration::from_secs(10)))?;
 
     loop {
         // Reset received byte counter.
         while socket.send(&[2])? != 1 {
             // Repeat until actually sent.
         }
-        let n = socket.recv(&mut buf)?;
+        let n = socket.read(&mut buf)?;
         let bytes_received = u64::from_le_bytes(buf[..n].try_into()?);
         if bytes_received == 0 {
             break;
@@ -59,30 +78,33 @@ fn main() -> Result<()> {
     rng.fill_bytes(&mut buf);
     buf[0] = 0;
 
+    socket.set_nonblocking(true)?;
+    println!("Starting benchmark.");
+
     // Perform throughput measurements.
     let mut bytes_transmitted = 0;
-    let mut sends = VecDeque::new();
     let mut total_send_duration = Duration::ZERO;
     let mut total_sends = 0;
     let time_start = Instant::now();
     loop {
         let transmit_start = Instant::now();
-        let n = socket.send(&buf)? as u64;
+        match socket.send(&buf) {
+            Ok(n) => {
+                bytes_transmitted += n as u64;
+            }
+            Err(err) if err.kind() == std::io::ErrorKind::WouldBlock => {}
+            Err(err) => return Err(err.into()),
+        };
         let now = Instant::now();
-        bytes_transmitted += n;
         total_send_duration += now.duration_since(transmit_start);
         total_sends += 1;
 
-        sends.push_back((transmit_start, n, now));
-        while let Some(&(transmit_start, _, _)) = sends.front() {
-            if now.duration_since(transmit_start) > Duration::from_secs(1) {
-                sends.pop_front();
-            } else {
-                break;
-            }
-        }
+        // Calculating sleep duration.
         let time_per_send = total_send_duration / total_sends;
-        let target_sends_per_second = args.bytes_per_second.0 / args.packet_size.0;
+        let mut target_sends_per_second = args.rate / args.packet_size.0;
+        if target_sends_per_second == 0 {
+            target_sends_per_second = 1;
+        }
         let sleep_time = (Duration::from_secs(1) / (target_sends_per_second as u32))
             .saturating_sub(time_per_send);
         if (now + sleep_time).duration_since(time_start) >= args.duration {
@@ -92,20 +114,31 @@ fn main() -> Result<()> {
     }
     let time_end = Instant::now();
 
+    socket.set_nonblocking(false)?;
+
     while socket.send(&[1])? != 1 {
         // Repeat until actually sent.
     }
 
-    println!("Waiting for bytes-received response...");
-    let n = socket.recv(&mut buf)?;
+    println!("Benchmark finished. Waiting for bytes-received response...");
+    let n = socket.read(&mut buf)?;
     let bytes_received = u64::from_le_bytes(buf[..n].try_into()?);
 
-    let loss = 1.0 - (bytes_received as f64) / (bytes_transmitted as f64);
+    let loss_absolute = if bytes_transmitted >= bytes_received {
+        bytesize::to_string(bytes_transmitted - bytes_received, true)
+    } else {
+        format!(
+            "-{}",
+            bytesize::to_string(bytes_received - bytes_transmitted, true)
+        )
+    };
+    let loss_relative = 1.0 - (bytes_received as f64) / (bytes_transmitted as f64);
     println!(
-        "Sent: {}, Received: {}, Loss: {:.1}%",
+        "Sent: {}, Received: {}, Loss: {} ({:.1}%)",
         bytesize::to_string(bytes_transmitted, true),
         bytesize::to_string(bytes_received, true),
-        loss * 100.0,
+        loss_absolute,
+        loss_relative * 100.0,
     );
     let duration = time_end.duration_since(time_start);
     println!(
@@ -118,6 +151,8 @@ fn main() -> Result<()> {
 }
 
 fn format_throughput(elapsed: Duration, bytes: u64) -> String {
-    let bytes_per_second = ((bytes as f64) / elapsed.as_secs_f64()) as u64;
-    format!("{}/s", bytesize::to_string(bytes_per_second, true))
+    let bits_per_second = (((bytes * 8) as f64) / elapsed.as_secs_f64()) as u64;
+    let mut s = bytesize::to_string(bits_per_second, false);
+    s.pop();
+    format!("{}bit/s", s)
 }
